bb7016aea52149f905ed35977b2acd130c04c2ac
Ruzimukhammad <ibrokhimovrozimuhammad@gmail.com>
2023-08-26 11:35:17 +0500

    AppaAsset.php changed. Css and Js links added

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,9 +12,35 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+//        '/css/site.css',
+        '/css/bootstrap.min.css',
+        '/css/font-awesome.min.css',
+        '/css/bootstrap-grid.min.css',
+        '/css/bootstrap-reboot.min.css',
+        '/css/font-techmarket.css',
+        '/css/slick.css',
+        '/css/techmarket-font-awesome.css',
+        '/css/slick-style.css',
+        '/css/animate.min.css',
+        '/css/colors/blue.css',
+        "https://fonts.googleapis.com/css?family=Rubik:300,400,500,900",
+        '/css/style.css',
     ];
     public $js = [
+        "/js/jquery.min.js",
+        "/js/tether.min.js",
+        "/js/bootstrap.min.js",
+        "/js/jquery-migrate.min.js",
+        "/js/hidemaxlistitem.min.js",
+        "/js/jquery-ui.min.js",
+        "/js/hidemaxlistitem.min.js",
+        "/js/jquery.easing.min.js",
+        "/js/scrollup.min.js",
+        "/js/jquery.waypoints.min.js",
+        "/js/waypoints-sticky.min.js",
+        "/js/pace.min.js",
+        "/js/slick.min.js",
+        "/js/scripts.js",
     ];
     public $depends = [
         'yii\web\YiiAsset',
