16b8ee953261e7eb8cbbf45f77862f331a545575
Ruzimukhammad <ibrokhimovrozimuhammad@gmail.com>
2023-08-30 15:45:44 +0500

    check

---
+++ frontend/views/category/index.php
@@ -0,0 +1,37 @@
+<?php
+
+use common\models\Category;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\widgets\ListView;
+use yii\widgets\Pjax;
+/** @var yii\web\View $this */
+/** @var common\models\CategorySearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Categories';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= ListView::widget([
+        'dataProvider' => $dataProvider,
+        'itemOptions' => ['class' => 'item'],
+        'itemView' => function ($model, $key, $index, $widget) {
+            return Html::a(Html::encode($model->name), ['view', 'id' => $model->id]);
+        },
+    ]) ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
